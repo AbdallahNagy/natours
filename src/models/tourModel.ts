a6cb9326be1cf241a9e486981c064da7c2858bea
abdallah nagy <abdallahnagy773@gmail.com>
2026-05-02 10:52:45 +0300

    add inexes

--- src/models/tourModel.ts
+++ src/models/tourModel.ts
@@ -144,6 +144,9 @@ const tourSchema = new Schema<ITour>(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
